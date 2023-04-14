fd9d12ca4d8be5310bf473d6813aeb58819a098e
Nick Abate <nick.abate@outlook.com>
2023-04-13 23:03:08 -0400

    implement form controlled components

--- src/routes/SignIn/SignIn.js
+++ src/routes/SignIn/SignIn.js
@@ -1,9 +1,26 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+
 import {
+  // auth,
   signInWithGooglePopup,
+  // signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/Firebase";
 
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+
 const SignIn = () => {
+  //   useEffect(() => {
+  //     const reDirect = async () => {
+  //       const response = await getRedirectResult(auth);
+  //       if (response) {
+  //         const userDocRef = await createUserDocumentFromAuth(response.user);
+  //       }
+  //     };
+  //     reDirect();
+  //   }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -13,6 +30,10 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
+      <SignUpForm />
     </div>
   );
 };
