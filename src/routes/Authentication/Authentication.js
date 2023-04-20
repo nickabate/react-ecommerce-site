b4d19910071a888b53b0f2bc37dc09ace168072b
Nick Abate <nick.abate@outlook.com>
2023-04-20 09:36:30 -0400

    add sign in and refactor authentication page

--- src/routes/Authentication/Authentication.js
+++ src/routes/Authentication/Authentication.js
@@ -1,16 +1,12 @@
 // import { useEffect } from "react";
 // import { getRedirectResult } from "firebase/auth";
 
-import {
-  // auth,
-  signInWithGooglePopup,
-  // signInWithGoogleRedirect,
-  createUserDocumentFromAuth,
-} from "../../utils/Firebase";
-
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import SignInForm from "../../components/SignInForm/SignInForm";
 
-const SignIn = () => {
+import "./Authentication.scss";
+
+const Authentication = () => {
   //   useEffect(() => {
   //     const reDirect = async () => {
   //       const response = await getRedirectResult(auth);
@@ -21,21 +17,17 @@ const SignIn = () => {
   //     reDirect();
   //   }, []);
 
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    <div className="authentication-container">
+      {/* <button onClick={logGoogleUser}>Sign in with Google Popup</button> */}
       {/* <button onClick={signInWithGoogleRedirect}>
         Sign in with Google Redirect
       </button> */}
+
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
