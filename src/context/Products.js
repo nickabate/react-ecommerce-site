376b2ddb55cbf40cc5a14045f4940d81f19a8bb3
Nick Abate <nick.abate@outlook.com>
2023-05-01 11:34:55 -0400

    retrieve categories map from firestore

--- src/context/Products.js
+++ src/context/Products.js
@@ -3,6 +3,8 @@ import { createContext, useState, useEffect } from "react";
 // eslint-disable-next-line
 // import { addCollectionAndDocuments } from "../utils/Firebase";
 
+import { getCategoriesAndDocuments } from "../utils/Firebase";
+
 // shop data only needed to interact with firestore db from our frontend - used to populate our db
 // import SHOP_DATA from "../data/shop-data";
 
@@ -12,6 +14,14 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
 
   // useEffect to populate our firestore db - only need to run once to populate
   // useEffect(() => {
